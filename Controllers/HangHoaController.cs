526ab575fb684f08589d8d740cdda17fc2cd7d16
hienlth <hyhien@gmail.com>
2018-10-27 17:33:55 +0700

    last commit on 27Oct2018

---
+++ Controllers/HangHoaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using K42Store.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace K42Store.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly MyeStoreContext db;
+        public HangHoaController(MyeStoreContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index(int? id)
+        {
+            List<HangHoa> dsHangHoas = new List<HangHoa>();
+            if (id.HasValue)
+            {
+                dsHangHoas = db.HangHoa.Where(p => p.MaLoai == id)
+                    .ToList();
+            }
+            else
+            {
+                dsHangHoas = db.HangHoa.ToList();
+            }
+            return View(dsHangHoas.Select(p=> new HangHoaViewModel
+            {
+                MaHh = p.MaHh, TenHh = p.TenHh, Hinh = p.Hinh,
+                DonGia = p.DonGia.Value, GiamGia = p.GiamGia
+            }));
+        }
+    }
+}
---
+++ Models/HangHoaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace K42Store.Models
+{
+    public class HangHoaViewModel
+    {
+        [Key]
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public double GiamGia { get; set; }
+        public double GiaBan => DonGia * (1 - GiamGia);
+        public bool DangKhuyenMai => GiamGia > 0;
+    }
+}
